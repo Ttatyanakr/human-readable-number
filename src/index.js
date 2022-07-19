f06c233f84ec7e2e7bd8dcf4198d0f96287f7fd7
Ttatyanakr <49091299+Ttatyanakr@users.noreply.github.com>
2022-07-19 06:27:04 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const dozens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundreds = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
   
-}
+    if (number < 20) {
+        return numbers[number];
+    } else if (number < 100 && number % 10 === 0) {
+        return dozens[+number.toString()[0]];
+    } else if (number < 100) {
+        return `${dozens[+number.toString()[0]]} ${numbers[+number.toString()[1]]}`;
+    } else if (number % 100 === 0) {
+        return `${hundreds[+number.toString()[0]]}`;
+    } else if (number > 100 && number % 10 === 0) {
+        return `${hundreds[+number.toString()[0]]} ${dozens[+number.toString()[1]]}`;
+    } else if (number > 100 && +number.toString()[1] === 1) {
+        return `${hundreds[+number.toString()[0]]} ${numbers[+number.toString().slice(1)]}`;
+    } else if (number > 100 && +number.toString()[1] === 0) {
+        return`${hundreds[+number.toString()[0]]} ${numbers[+number.toString()[2]]}`;
+    } else {
+        return `${hundreds[+number.toString()[0]]} ${dozens[+number.toString()[1]]} ${numbers[+number.toString()[2]]}`;
+    }
+  }
